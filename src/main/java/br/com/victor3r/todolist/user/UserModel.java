92fab7fca67ecf32bae7ec915bffc6d527e15c87
Victor Rabelo <victorabelo1697@gmail.com>
2023-10-12 17:53:08 -0300

    init app

---
+++ src/main/java/br/com/victor3r/todolist/user/UserModel.java
@@ -0,0 +1,7 @@
+package br.com.victor3r.todolist.user;
+
+public class UserModel {
+  public String username;
+  public String name;
+  public String password;
+}
---
+++ src/test/java/br/com/victor3r/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victor3r.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
