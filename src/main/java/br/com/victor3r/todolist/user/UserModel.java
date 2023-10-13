7aabf61522fe5e9983310f68ee5af536279d386a
Victor Rabelo <victorabelo1697@gmail.com>
2023-10-13 20:10:44 -0300

    database connection

--- src/main/java/br/com/victor3r/todolist/user/UserModel.java
+++ src/main/java/br/com/victor3r/todolist/user/UserModel.java
@@ -1,7 +1,34 @@
 package br.com.victor3r.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "users")
 public class UserModel {
-  public String username;
-  public String name;
-  public String password;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @Column(unique = true)
+  private String username;
+  private String name;
+  private String password;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
 }
